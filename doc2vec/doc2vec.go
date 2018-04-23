5245d0b9e96668105dfa9f8bf45e42eb6c928de2
Graham Clark <gclark@extremenetworks.com>
2018-04-22 23:09:58 -0400

    Skip newlines

--- doc2vec/doc2vec.go
+++ doc2vec/doc2vec.go
@@ -691,7 +691,7 @@ func (p *TDoc2VecImpl) trainSkipGram() {
 				if last_trained_words > PROGRESS_BAR_THRESHOLD {
 					last_trained_words = 0
 					alpha = p.getTrainAlpha()
-					logrus.Infof("%cSkip-Gram Iter:%v Alpha: %f  Progress: %.2f%%  Words/sec: %.2fk  ", 13, i, alpha,
+					logrus.Infof("Skip-Gram Iter:%v Alpha: %f  Progress: %.2f%%  Words/sec: %.2fk  ", i, alpha,
 						float64(p.TrainedWords)/float64(p.Iters*p.Corpus.GetWordsCnt()+1)*100,
 						float64(p.TrainedWords)/float64(time.Since(stime))*100*1000)
 				}
@@ -726,7 +726,7 @@ func (p *TDoc2VecImpl) trainCbow() {
 				if last_trained_words > PROGRESS_BAR_THRESHOLD {
 					last_trained_words = 0
 					alpha = p.getTrainAlpha()
-					logrus.Infof("%cCBOW Iter:%v Alpha: %f  Progress: %.2f%%  Words/sec: %.2fk  ", 13, i, alpha,
+					logrus.Infof("CBOW Iter:%v Alpha: %f  Progress: %.2f%%  Words/sec: %.2fk  ", i, alpha,
 						float64(p.TrainedWords)/float64(p.Iters*p.Corpus.GetWordsCnt()+1)*100,
 						float64(p.TrainedWords)/float64(time.Since(stime))*100*1000)
 				}
